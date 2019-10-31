91089575b4073b42d1302fe7dfbfdd00b3ecca46
Mohammad Ridwan Hady Arifin <ridwanhady31@gmail.com>
2019-10-31 15:09:33 +0700

    Fixing Infinite Loop

--- txn/txn_processor.cc
+++ txn/txn_processor.cc
@@ -255,13 +255,12 @@ void TxnProcessor::ApplyWrites(Txn* txn) {
 void TxnProcessor::RunOCCScheduler() {
   Txn* txn;
   while (tp_.Active()) {
-    //Get the next new transaction request (if one is pending) and pass it to an execution thread.
-    //!!! Read Phase !!!
     if (txn_requests_.Pop(&txn)) {
       ExecuteTxn(txn);
       bool valid = true;
       for (Key record : txn->readset_) {
-        if (storage_->Timestamp(record) > txn->occ_start_time_){//the record was last updated AFTER this transactions start time) {
+        //the record was last updated AFTER this transactions start time)
+        if (storage_->Timestamp(record) > txn->occ_start_time_){
           valid = false;
         }
       }
@@ -287,6 +286,7 @@ void TxnProcessor::RunOCCScheduler() {
         ApplyWrites(txn);
         txn->status_ = COMMITTED;
       }
+      txn_results_.Push(txn);
     }
   }
 }
